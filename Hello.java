48915dbe2296c0ad39550ea2ec95fae62a1c15f6
Namory Traore <namory.traore@ericsson.com>
2019-01-10 17:27:04 -0500

    added comment

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
+		//dicsplay number from 1 to 10
 		for(int i = 0; i <= 10; i++)
 		{
 			System.out.println("Hello world... " + i);
